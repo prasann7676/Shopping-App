e32debf4924667d9fcd9f6e3901f41c21db16693
Prasann Kumar Gupta <prasanngupta7676@gmail.com>
2023-04-06 17:00:50 +0530

    Revert "Shopping App Project"
    This reverts commit cd0777e847d8dd0168e3bfff4ac15f940faa1ef1.

--- backend/middleware/catchAsyncErrors.js
+++
@@ -1,6 +0,0 @@
-// this file is responsible for handling async await errors.
-module.exports = theFunc => (req,res,next) => {
-    //Promise is a predefines class
-    //resolve can be said as the then block and catch is catch
-    Promise.resolve(theFunc(req,res,next)).catch(next)
-}
